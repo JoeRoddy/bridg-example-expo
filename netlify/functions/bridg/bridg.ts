d2dc941f27d03bcb6434d2d3cf476c12a644026c
Joe Roddy <mrjoeroddy@gmail.com>
2023-05-02 21:02:22 -0400

    add prisma

--- netlify/functions/bridg/bridg.ts
+++ netlify/functions/bridg/bridg.ts
@@ -1,11 +1,12 @@
 import { Handler } from '@netlify/functions';
 import { PrismaClient } from '@prisma/client';
-import { handleRequest } from 'bridg/app/server/request-handler';
+// import { handleRequest } from 'bridg/app/server/request-handler';
 
 const prisma = new PrismaClient();
 
 export const handler: Handler = async (event) => {
-  const body = JSON.parse(event.body as string);
-  const { data, status } = await handleRequest(body, { db: prisma, rules: { default: true } });
-  return { statusCode: status, body: JSON.stringify(data) };
+  return { statusCode: 200 };
+  // const body = JSON.parse(event.body as string);
+  // const { data, status } = await handleRequest(body, { db: prisma, rules: { default: true } });
+  // return { statusCode: status, body: JSON.stringify(data) };
 };
--- netlify/functions/hello/hello.ts
+++ netlify/functions/hello/hello.ts
@@ -1,12 +1,14 @@
-import { Handler } from '@netlify/functions'
+import { PrismaClient } from '@prisma/client';
+import { Handler } from '@netlify/functions';
+
+const db = new PrismaClient();
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  // const { name = 'stranger' } = event.queryStringParameters
 
+  const data = await db.user.findMany();
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
-  }
-}
+    body: JSON.stringify(data),
+  };
+};
