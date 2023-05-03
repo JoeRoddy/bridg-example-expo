7e798657ad38d5b9372633e79c621549e959a4d4
Joe Roddy <mrjoeroddy@gmail.com>
2023-05-02 21:04:41 -0400

    example bridg query

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useAsync } from './hooks/useAsync';
+import db from 'bridg/app/client/db';
 
 export default function App() {
-  const [data] = useAsync(() => fetch(`/.netlify/functions/hello`).then((r) => r.json()));
-
-  console.log('data:', data);
+  const [data] = useAsync(() => db.user.findMany({ include: { blogs: true } }));
 
   return (
     <View style={styles.container}>
--- netlify/functions/bridg/bridg.ts
+++ netlify/functions/bridg/bridg.ts
@@ -1,12 +1,11 @@
 import { Handler } from '@netlify/functions';
 import { PrismaClient } from '@prisma/client';
-// import { handleRequest } from 'bridg/app/server/request-handler';
+import { handleRequest } from 'bridg/app/server/request-handler';
 
 const prisma = new PrismaClient();
 
 export const handler: Handler = async (event) => {
-  return { statusCode: 200 };
-  // const body = JSON.parse(event.body as string);
-  // const { data, status } = await handleRequest(body, { db: prisma, rules: { default: true } });
-  // return { statusCode: status, body: JSON.stringify(data) };
+  const body = JSON.parse(event.body as string);
+  const { data, status } = await handleRequest(body, { db: prisma, rules: { default: true } });
+  return { statusCode: status, body: JSON.stringify(data) };
 };
